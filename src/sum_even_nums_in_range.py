5f1d811303109515d8510aa19dce6b777dc3aac6
zhyzh <Zhyzhiimisha17@gmail.com>
2024-02-29 18:44:35 +0200

    ask 9 dan

--- src/sum_even_nums_in_range.py
+++ src/sum_even_nums_in_range.py
@@ -1,3 +1,7 @@
 def sum_even_nums_in_range(start, stop):
-  # Type your code
-  return
+  return sum([num for num in range(start, stop + 1) if num % 2 == 0])
+
+
+# Приклад використання:
+result = sum_even_nums_in_range(1, 10)
+print(result)  # Виведе суму парних чисел від 1 до 10, тобто 2 + 4 + 6 + 8 + 10 = 30
