345900c1b76e22e86e865f66710a02bd06fb5b98
zhyzh <Zhyzhiimisha17@gmail.com>
2024-02-29 18:46:27 +0200

    ask 2 dan

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,3 +1,15 @@
 def fizz_buzz(param):
   #Type your code here
-  return
+  result = ""
+  if param % 3 == 0:
+    result += "Fizz"
+  if param % 5 == 0:
+    result += "Buzz"
+  if not result:
+    result = str(param)
+  return result
+
+print(fizz_buzz(10))  #Виведе "Buzz"
+print(fizz_buzz(6))   #Виведе "Fizz"
+print(fizz_buzz(15))  #Виведе "FizzBuzz"
+print(fizz_buzz(7))   #Виведе число 7
