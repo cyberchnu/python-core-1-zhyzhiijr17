a5cb16e8750009ef8ea3c7524ce9f4715f7f4463
zhyzh <Zhyzhiimisha17@gmail.com>
2024-02-29 18:45:20 +0200

    ask 3 dan

--- src/tri_area.py
+++ src/tri_area.py
@@ -1,3 +1,8 @@
 def tri_area(base, height):
-  # Type your code
-  return 
+    return (base * height) / 2
+
+
+base_length = 5
+height_length = 8
+area = tri_area(base_length, height_length)
+print("Площа трикутника з основою", base_length, "та висотою", height_length, "дорівнює", area)
