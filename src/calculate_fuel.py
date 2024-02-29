33f21f3c713ba63b4635857f3e3223582f4568bb
zhyzh <Zhyzhiimisha17@gmail.com>
2024-02-29 18:47:04 +0200

    ask 4 dan

--- src/calculate_fuel.py
+++ src/calculate_fuel.py
@@ -1,3 +1,13 @@
 def calculate_fuel(distance):
-  # Type your code
-  return
+    if distance <= 0:
+        return 100  # Повертаємо 100, якщо відстань менша або дорівнює нулю
+    else:
+        fuel_needed = distance * 10
+        if fuel_needed < 100:
+            return 100  # Повертаємо 100, якщо розраховане значення менше за 100
+        else:
+            return fuel_needed
+
+
+distance_to_travel = 20  # Приклад відстані в кілометрах
+required_fuel = calculate_fuel(distance_to_travel)
