a1f051fc536fb704d2f6d9d2841cebf3a9b0c730
zhyzh <Zhyzhiimisha17@gmail.com>
2024-02-29 18:43:49 +0200

    ask 8 dan

--- src/squares_sum.py
+++ src/squares_sum.py
@@ -1,3 +1,6 @@
 def squares_sum(n):
-  # Type your code
-  return
+    return sum([i ** 2 for i in range(1, n + 1)])
+
+
+result = squares_sum(5)
+print(result)  # Виведе 1^2 + 2^2 + 3^2 + 4^2 + 5^2 = 55
