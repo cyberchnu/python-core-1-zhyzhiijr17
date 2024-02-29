41d48a349e41d6b0dadc603ddc0beefc8d1f74db
zhyzh <Zhyzhiimisha17@gmail.com>
2024-02-29 18:42:26 +0200

    ask 10 dan

--- src/mean.py
+++ src/mean.py
@@ -1,3 +1,10 @@
 def mean(number):
-  # Type your code
-  return 
+    # Перетворюємо число у рядок, щоб можна було ітерувати по його цифрам
+    digits = [int(digit) for digit in str(number)]
+    # Обчислюємо середнє значення
+    mean_value = sum(digits) / len(digits)
+    return mean_value
+
+
+result = mean(12345)
+print(result)  # Виведе середнє значення цифр у числі 12345, тобто (1 + 2 + 3 + 4 + 5) / 5 = 3.0
