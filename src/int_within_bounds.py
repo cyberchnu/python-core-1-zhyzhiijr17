343672764cdc73941e330df931523535f3bc63fa
zhyzh <Zhyzhiimisha17@gmail.com>
2024-02-29 18:34:30 +0200

    ask 6 dan

--- src/int_within_bounds.py
+++ src/int_within_bounds.py
@@ -1,3 +1,11 @@
 def int_within_bounds(number, lower_bound, upper_bound):
-  # Type your code
-  return 
+    if isinstance(number, int) and isinstance(lower_bound, int) and isinstance(upper_bound, int):
+        return lower_bound <= number < upper_bound
+    else:
+        return False
+
+
+print(int_within_bounds(10, 5, 20))  # Поверне True
+print(int_within_bounds(5, 5, 20))  # Поверне True
+print(int_within_bounds(25, 5, 20))  # Поверне False
+print(int_within_bounds(15.5, 10, 20))  # Поверне False
