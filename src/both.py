bd2152e38ab7bf2bf6292e400e6e4967ffb9a4dd
zhyzh <Zhyzhiimisha17@gmail.com>
2024-02-29 18:52:42 +0200

    ask 5 dan

--- src/both.py
+++ src/both.py
@@ -1,3 +1,5 @@
 def both(number1, number2):
-  # Type your code
-  return
+    if number1 < 0 and number2 < 0 or number1 == 0 and number2 == 0 or number1 > 0 and number2 > 0:
+        return True
+    else:
+        return False
