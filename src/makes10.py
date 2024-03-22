6509ce0f7d7b50b3d431bfa28f420348fb1b6844
zhyzh <Zhyzhiimisha17@gmail.com>
2024-03-22 10:18:57 +0200

    task 10 done

--- src/makes10.py
+++ src/makes10.py
@@ -1,5 +1,7 @@
 def makes10(number1, number2):
-    if number1 == 10 or number2 == 10 or number1 + number2 == 10:
-        return True
+  if number1 == 10 or number2 == 10 or number1 + number2 == 10:
+    return True
+  else:
+    return False
 
 print(makes10(1, 0))
